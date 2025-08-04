47e8d69539fcdbb6142abc3885651da9c1b808b2
gradippp <me@agradip.fyi>
2025-08-04 06:23:26 +0530

    Better footer n body backgrounds

--- src/components/EditorFooter.tsx
+++ src/components/EditorFooter.tsx
@@ -112,7 +112,7 @@ export default function EditorFooter({
 
   return (
     <footer
-      className="fixed bottom-0 right-0 h-12 w-full bg-gray-900 px-4 py-2 flex justify-between items-center gap-2 shadow z-20"
+      className="fixed bottom-0 right-0 h-12 w-full bg-gray-950 text-gray-200 border-t border-gray-800 px-4 py-2 flex justify-between items-center gap-2 shadow z-20"
       style={{}}
     >
       <div className="flex items-center gap-2">
